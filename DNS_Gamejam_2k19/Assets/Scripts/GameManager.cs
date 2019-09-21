281bab7a63895059f7dc2375ea9b297a1a215511
Jakub Kazimierski <jakubkazimierski622@gmail.com>
2019-09-21 14:20:52 +0200

    Adds scene management in GameManager and removes unnessesary parts of it.

--- DNS_Gamejam_2k19/Assets/Scripts/GameManager.cs
+++ DNS_Gamejam_2k19/Assets/Scripts/GameManager.cs
@@ -1,54 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static ulong HighScore = 0;
-    public ulong CurrentScore = 0,
-        PointsPerSecond = 10;
+    public GameState CurrentState = GameState.MAIN_MENU;
+    public string[] SceneNames;
 
-    float dtime = 0.0f;
+    static GameManager instance;
+
+    public enum GameState
+    {
+        MAIN_MENU,
+        GAME,
+        CREDITS,
+        END
+    }
 
     void Start()
     {
+    }
 
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
     {
-        UpdateScoreOverTime();
+        SceneHandling();
     }
 
-    public void OnCoinPickUp(int value)
+    void SceneHandling()
     {
-        CurrentScore += (ulong)value;
-    }
+        var ActualSceneName = SceneManager.GetActiveScene().name;
 
-    public void RecordHighScore()
-    {
-        if(CurrentScore > HighScore)
+        if (ActualSceneName != SceneNames[(int)CurrentState])
         {
-            HighScore = CurrentScore;
-        }
-    }
+            SceneManager.LoadScene(SceneNames[(int)CurrentState]);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(SceneNames[(int)CurrentState]));
 
-    public void RestartScore()
-    {
-        CurrentScore = 0;
-        dtime = 0.0f;
-    }
-
-    void UpdateScoreOverTime()
-    {
-        if (dtime < 1.0f)
-        {
-            dtime += Time.deltaTime;
-        }
-        else
-        {
-            CurrentScore += PointsPerSecond;
-            dtime = 0.0f;
+            //SceneManager.UnloadSceneAsync(ActualSceneName);
         }
     }
 }
