d78290e2eceff0248c4c46635ad228e39f048d74
Jakub Kazimierski <jakubkazimierski622@gmail.com>
2019-09-21 12:02:32 +0200

    Adds simplistic score system.

---
+++ DNS_Gamejam_2k19/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static ulong HighScore = 0;
+    public ulong CurrentScore = 0,
+        PointsPerSecond = 10;
+
+    float dtime = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        UpdateScoreOverTime();
+    }
+
+    public void OnCoinPickUp(int value)
+    {
+        CurrentScore += (ulong)value;
+    }
+
+    public void RecordHighScore()
+    {
+        if(CurrentScore > HighScore)
+        {
+            HighScore = CurrentScore;
+        }
+    }
+
+    public void RestartScore()
+    {
+        CurrentScore = 0;
+        dtime = 0.0f;
+    }
+
+    void UpdateScoreOverTime()
+    {
+        if (dtime < 1.0f)
+        {
+            dtime += Time.deltaTime;
+        }
+        else
+        {
+            CurrentScore += PointsPerSecond;
+            dtime = 0.0f;
+        }
+    }
+}
